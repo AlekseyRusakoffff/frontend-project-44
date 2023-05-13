5fd05ddb90304b9f633cf89c1c74d3b443ecb9d1
Aleksey Rusakov <https://github.com/AlekseyRusakoffff/AlekseyRusakoffff.github.io>
2023-05-13 14:07:12 +0300

    4,4

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 // Wait for user's response.
 // eslint-disable-next-line no-unused-vars
 const userName = readlineSync.question('May I have your name? ');
-console.log("Hi ' + userName + '!");
+console.log(`Hi ${userName}!`);
