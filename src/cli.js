646212549822a2b9ed03a4588d60b2a4a1ba862b
Aleksey Rusakov <https://github.com/AlekseyRusakoffff/AlekseyRusakoffff.github.io>
2023-08-03 13:46:36 +0300

    9,9

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
-
+console.log('Welcome to the Brain Games!');
+export const sayNameHello = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+
+  return userName;
 };
+const userName = sayNameHello();
+console.log(`Hello, ${userName}!`);
+export default sayNameHello;
